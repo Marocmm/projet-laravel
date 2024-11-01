1f30647055ef52a3597d7fd92c8036eb5601bb70
Khadijaelali <155186710+Khadijaelali@users.noreply.github.com>
2024-11-01 17:48:51 +0100

    routes configur

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::resource('contact', ContactController::class);
+Route::get('/contact/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('articles.show');
+Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+Route::put('/contact/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
