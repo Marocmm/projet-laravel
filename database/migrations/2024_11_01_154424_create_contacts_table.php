4cc13d93772dff203a975104c64a928489e83c4e
Yassine <yassinerlrha020@gmail.com>
2024-11-01 17:19:15 +0100

    tt

--- database/migrations/2024_11_01_154424_create_contacts_table.php
+++ database/migrations/2024_11_01_154424_create_contacts_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            
-
             $table->timestamps();
         });
     }
