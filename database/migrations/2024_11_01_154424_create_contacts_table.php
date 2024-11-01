c817558e82fde356de3f2254548d2ff4c4c9a777
Khadijaelali <155186710+Khadijaelali@users.noreply.github.com>
2024-11-01 17:11:48 +0100

    model  de table

--- database/migrations/2024_11_01_154424_create_contacts_table.php
+++ database/migrations/2024_11_01_154424_create_contacts_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            
+            $table -> string('nom');
+            $table-> string('email');
+            $table -> string('telephone');
+                    
 
             $table->timestamps();
         });
